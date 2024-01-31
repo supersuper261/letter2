81fca5c41af2bd89ad3daeec91b99189a51dceea
supersuper261 <148677532+supersuper261@users.noreply.github.com>
2024-01-31 20:48:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const envelope = document.getElementById("envelope");
-  const flap = document.querySelector(".envelope-flap");
-
-  setTimeout(() => {
-    flap.style.top = "-80px";
-  }, 1000);
-
-  setTimeout(() => {
-    envelope.classList.add("hidden");
-  }, 2000);
+document.getElementById('envelope').addEventListener('click', function() {
+    this.classList.add('open');
 });
